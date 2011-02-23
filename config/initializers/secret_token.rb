7d1c624e021d37c95ff98c9a4a06b361d6b33e9c
sharkkato103 <michael.murphy@students.olin.edu>
2011-02-23 09:36:29 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EchnoestSprintRails::Application.config.secret_token = 'a4f74b726cf788c0f9fd0f23f3db3881dd8b65b984cf72e254b797bdd2e16c8b4e844e5647a66f22fb314c62f9fa5a52b5ce0e3c7e3a093419e436e9f0bddd53'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EchnoestSprintRails::Application.config.session_store :cookie_store, :key => '_echnoest-sprint-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EchnoestSprintRails::Application.config.session_store :active_record_store
